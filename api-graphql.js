c13842752f8bcddcbe7e7942ccebbaf21dc808aa
François Hodierne <francois@hodierne.net>
2020-06-06 22:48:48 +0200

    graphql pipeline 2

--- api-graphql.js
+++ api-graphql.js
@@ -1,13 +1,29 @@
+const hyperwatch = require('@hyperwatch/hyperwatch');
+const chalk = require('chalk');
 const dotenv = require('dotenv');
+const { pick } = require('lodash');
+
+const { app, pipeline, input, plugins, modules, start, lib, util } = hyperwatch;
+
+const { cloudflare, hostname, dnsbl, geoip, useragent, identity } = plugins;
+
+// Load config
 
 dotenv.config();
 
-const chalk = require('chalk');
-const hyperwatch = require('@hyperwatch/hyperwatch');
+// Add Open Collective specific regexes
 
-const { pipeline, input, plugins, modules, start, util } = hyperwatch;
+lib.useragent.addRegex('robot', {
+  regex: '(opencollective-images)/(\\d+)\\.(\\d+)',
+  family_replacement: 'Images', // eslint-disable-line camelcase
+});
 
-const { cloudflare, hostname, dnsbl, geoip } = plugins;
+lib.useragent.addRegex('robot', {
+  regex: '(opencollective-frontend)/(\\d+)\\.(\\d+)',
+  family_replacement: 'Frontend', // eslint-disable-line camelcase
+});
+
+// Connect Input
 
 const websocketClientInput = input.websocket.create({
   name: 'WebSocket client (JSON standard format)',
@@ -25,33 +41,74 @@ const websocketClientInput = input.websocket.create({
 
 pipeline.registerInput(websocketClientInput);
 
+// Setup Pipeline and data augmentation
+
 pipeline
   .map((log) => cloudflare.augment(log))
   .map((log) => hostname.augment(log))
-  // .map((log) => hostname.meta(log))
   .map((log) => dnsbl.augment(log))
   .map((log) => geoip.augment(log))
+  .map((log) => useragent.augment(log))
+  .map((log) => identity.augment(log))
+  .map((log) => {
+    if (log.getIn(['request', 'headers', 'oc-application']) === 'frontend') {
+      // check secret
+      log = log.set('application', 'frontend');
+      log = log.set('identity', 'frontend');
+    }
+    if (log.getIn(['request', 'headers', 'oc-application']) === 'images') {
+      // check secret
+      log = log.set('application', 'images');
+      log = log.set('identity', 'images');
+    }
+    if (log.getIn(['request', 'headers', 'oc-application']) === 'rest') {
+      // check secret
+      log = log.set('application', 'rest');
+      log = log.set('identity', 'rest');
+    }
+    if (log.hasIn(['opencollective', 'user', 'email'])) {
+      log = log.set('identity', log.getIn(['opencollective', 'user', 'email']));
+    }
+    return log;
+  })
+  .registerNode('main')
   .filter((log) => log.has('graphql'))
-  .registerNode('main');
+  .registerNode('graphql');
 
+// Register application nodes
+
+let node, other;
 for (const application of ['frontend', 'images', 'rest']) {
-  pipeline
-    .getNode('main')
-    .filter(
-      (log) =>
-        log.getIn(['request', 'headers', 'oc-application']) === application,
-    )
-    .registerNode(application);
+  [node, other] = (other || pipeline.getNode('main')).split(
+    (log) =>
+      log.getIn(['request', 'headers', 'oc-application']) === application,
+  );
+  node.registerNode(application);
 }
+other.registerNode('other');
 
-pipeline.getNode('main').map((log) => {
-  const application = log.getIn(['request', 'headers', 'oc-application']);
+// Register slow nodes
+
+pipeline
+  .getNode('graphql')
+  .filter((log) => log.get('executionTime') > 100)
+  .registerNode('slow');
+
+pipeline
+  .getNode('graphql')
+  .filter((log) => log.get('executionTime') > 1000)
+  .registerNode('extra-slow');
+
+// Log to the console
+
+pipeline.getNode('slow').map((log) => {
+  const identity = log.getIn(['identity']);
 
   console.log(
     chalk.red(log.getIn(['request', 'time']).slice(11)),
 
     chalk.white(
-      application ||
+      identity ||
         log.getIn(['hostname', 'value']) ||
         log.getIn(['address', 'value']),
     ),
@@ -71,61 +128,59 @@ pipeline.getNode('main').map((log) => {
   return log;
 });
 
-modules.addresses.setMapper((entry, format) => {
+// Add GraphQL aggregator
+
+const aggregator = new lib.aggregator.Aggregator();
+
+aggregator.setIdentifier(
+  (log) => `${log.getIn(['graphql', 'operationName']) || 'unknown'}`,
+);
+
+aggregator.setEnricher((entry, log) => {
+  if (log.has('graphql')) {
+    entry = entry.set('graphql', log.get('graphql'));
+  }
+  if (log.has('application')) {
+    entry = entry.set('application', log.get('application'));
+  }
+  return entry;
+});
+
+aggregator.setMapper((entry) => {
   return {
-    identity:
-      entry.get('identity') ||
-      entry.getIn(['opencollective', 'user', 'email']) ||
-      entry.getIn(['opencollective', 'application', 'id']),
-    hostname: entry.getIn(['address', 'hostname'])
-      ? `${entry.getIn(['address', 'hostname'])}${
-          entry.getIn(['hostname', 'verified']) ? '+' : ''
-        }`
-      : entry.getIn(['address', 'value']),
-    // type: entry.getIn(['address', 'type']),
-    // owner: entry.getIn(['address', 'owner']),
-    cc: entry.getIn(['geoip', 'country']),
-    reg: entry.getIn(['geoip', 'region']),
-    city: entry.getIn(['geoip', 'city']),
-    dc: entry.getIn(['cloudflare', 'data-center']),
+    operation: entry.getIn(['graphql', 'operationName']) || 'unknown',
+    application: entry.getIn(['application']),
     '15m': util.aggregateSpeed(entry, 'per_minute'),
     '24h': util.aggregateSpeed(entry, 'per_hour'),
-    xbl: entry.getIn(['dnsbl', 'xbl'])
-      ? format === 'txt'
-        ? ' x '
-        : true
-      : format === 'txt'
-      ? ''
-      : false,
   };
 });
 
-modules.addresses.setEnricher((entry, log) => {
-  for (const field of [
-    'address',
-    'hostname',
-    'cloudflare',
-    'geoip',
-    'dnsbl',
-    'opencollective',
-  ]) {
-    if (!entry.has(field) && log.has(field)) {
-      entry = entry.set(field, log.get(field));
-    }
-  }
+pipeline.getNode('graphql').map((log) => aggregator.processLog(log));
 
-  if (
-    !entry.has('identity') &&
-    log.hasIn(['request', 'headers', 'oc-application'])
-  ) {
-    entry = entry.set(
-      'identity',
-      log.getIn(['request', 'headers', 'oc-application']),
-    );
-  }
+app.api.registerAggregator('graphql', aggregator);
 
-  return entry;
+// Tweak Logs module
+
+modules.logs.setFormatter((log) => {
+  return `${log.getIn(['request', 'time']).slice(11)} ${
+    log.getIn(['identity']) ||
+    log.getIn(['hostname', 'value']) ||
+    log.getIn(['address', 'value']) ||
+    log.getIn(['request', 'address'])
+  } ${log.getIn(['graphql', 'operationName'])} ${JSON.stringify(
+    pick(log.getIn(['graphql', 'variables']).toJS(), [
+      'id',
+      'slug',
+      'collectiveSlug',
+      'CollectiveSlug',
+      'CollectiveId',
+      'legacyExpenseId',
+      'tierId',
+    ]),
+  )} ${log.get('executionTime')}ms`;
 });
+
+// Start
 
 modules.load();
 
